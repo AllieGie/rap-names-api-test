cadc8f1f1bc054ada2baea73fc77c47393cd8d70
Alliegie <100817906+AllieGie@users.noreply.github.com>
2022-06-08 09:43:04 -0400

    updated server.js files

--- server.js
+++ server.js
@@ -17,6 +17,81 @@ const rappers = {
         'birthName': 'Chancelor Bennett',
         'birthLocation': 'Chicago, Illinois'
     },
+    'kendrick lamar': {
+        'age': 34,
+        'birthName': 'Kendrick Lamar Duckworth',
+        'birthLocation':'Compton,California'
+    },
+    'jay-z': {
+        'age': 52,
+        'birthName': 'Shawn Corey Carter',
+        'birthLocation': 'New York City'
+    },
+    'da baby': {
+        'age': 30,
+        'birthName': 'Jonathan Lyndale Kirk',
+        'birthLocation': 'Cleveland, Ohio'
+    },
+    'big krit':{
+        'age': 35,
+        'birthName': 'Justin Lewis Scott',
+        'birthLocation': 'Meridian, Mississippi'
+    },
+    'pop smoke': {
+        'age': 20,
+        'borthName': 'Bashar Barakah Jackson',
+        'birthLocation': ' Canarsie, Brooklyn, New York'
+    },
+    'kevin gates': {
+        'age': 36,
+        'birthName': 'Kevin Jerome Gilyard',
+        'birthLocation': 'New Orleans, Louisiana'
+    },
+    'madeintyo': {
+        'age': 30,
+        'birthName': 'Malcolm Jamaal Davis',
+        'birthLocation': 'Honolulu, Hawaii'
+    },
+    'kodak black': {
+        'age': 24,
+        'birthName': 'Dieuson Octave',
+        'birthLocation': 'Pompano Beach, Florida'
+    },
+    'nipsey hussle': {
+        'age': 33,
+        'birthName': 'Airmiess Joseph Asghedom',
+        'birthLocation':'Crenshaw, Los Angeles, California'
+    }, 
+    'roddy ricch': {
+        'age': 23,
+        'birthName': 'Rodrick Wayne Moore Jr.',
+        'birthLocation':'Compton, California'
+    },
+    'freddie gibbs': {
+        'age': 39,
+        'birthName': 'Fredrick Jamel Tipton',
+        'birthLocation':'Gary, Indiana'
+    },
+    'Doechii': {
+        'age': 23,
+        'birthName': 'Jaylah Hickmon',
+        'birthLocation':'Tampa,Florida'
+    },
+    'isaiah rashad': {
+        'age': 31,
+        'birthName': 'Isaiah Joel McClain',
+        'birthLocation':'Chattanooga, Tennessee'
+    },
+    'noname': {
+        'age': 30,
+        'birthName': 'Fatimah Nyeema Warner',
+        'birthLocaton':'Bronzeville, Chigago'
+    },
+    'goldlink': {
+        'age': 29,
+        'birthName':'D\'Anthony William Carlos',
+        'birthLocation':'Washington, D.C.'
+    },
     'unknown': {
         'age': 0,
         'birthName': 'unknown',
@@ -24,8 +99,6 @@ const rappers = {
     }
 
 }
-
-
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
